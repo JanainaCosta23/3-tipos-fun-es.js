8acd19c991866056119c85e3b92eeb624889c544
JanainaCosta23 <148474386+JanainaCosta23@users.noreply.github.com>
2023-11-30 20:16:24 -0300

    funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+let soma1 = 3;
+let soma2 = 2;
+let mult1 = 4;
+let mult2 = 2;
+
+// Função sem parâmetro
+function helloworld() {
+    console.log("Resultados da Soma e da Multiplicação ");
+    // Console.log só para pular linha.
+    console.log()
+  }
+  
+  // Função com parâmetros para fazer a soma.
+  function soma(a, b) {
+    return a + b;
+  }
+  
+  // Função arrow com parâmetros para fazer a multiplicação.
+  const multiplicar = (a, b) => a * b;
+  
+  // Chamando as funções para serem exibidas.
+  helloworld();
+  console.log(soma1, '+',soma2,'=', soma(soma1, soma2));
+  console.log(mult1, '*',mult2,'=', multiplicar(mult1, mult2));
